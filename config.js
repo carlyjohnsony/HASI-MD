c9b007ad98fce1a29cae66805085b0d2f66c960a
carlyjohnsony <nipsykay48@gmail.com>
2024-12-25 07:58:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,27 +3,28 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
  
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://chhaseeb517:chhaseeb517@cluster0.1dtvf4q.mongodb.net/?retryWrites=true&w=majority"
+global.owner = process.env.OWNER_NUMBER.split("260776642930,")
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://nipsy:
+200518kay@cluster0.94tna.mongodb.net/"
 global.port= process.env.PORT || 5000
 global.email = 'infiniteytff@gmail.com'
 global.github = 'https://github.com/chhaseeb47/HASI-MD'
 global.location = 'Pakistan'
 global.gurl = 'https://instagram.com/_' // add your username
-global.sudo = process.env.SUDO || '923407472645'
+global.sudo = process.env.SUDO || '260760431115'
 global.devs = '923407472645';
 global.website = 'https://github.com/chhaseeb47/HASI-MD' // Hello world
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ea5cfd778d2004c72d965.jpg'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'HASI-MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? '𝗖𝗛 𝗛𝗔𝗦𝗘𝗘𝗕' : process.env.OWNER_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Nipusi' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? '𝗖𝗛 𝗛𝗔𝗦𝗘𝗘𝗕' : process.env.PACK_AUTHER.split(";")[0],
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_NAME.split(";")[1] === undefined ? 'HASI-MD' : process.env.PACK_NAME.split(";")[1],
+  author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? 'Nipsy's' : process.env.PACK_AUTHER.split(";")[0],
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
+  packname:  process.env.PACK_NAME.split(";")[1] === undefined ? 'Creation🔌' : process.env.PACK_NAME.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nigga' : process.env.ANTI_BAD_WORD,
-  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '212' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
@@ -35,7 +36,7 @@ module.exports = {
   antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
   BRANCH: 'main', 
   ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
-  autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
+  autobio:  process.env.AUTO_BIO === undefined ? true : process.env.AUTO_BIO,
   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
   heroku:  process.env.heroku === undefined ? false : process.env.heroku,
   HEROKU: {
@@ -45,7 +46,7 @@ module.exports = {
 },
   VERSION: process.env.VERSION === undefined ? 'Ｖ-1.1.１' : process.env.VERSION,
   LANG: process.env.THEME|| 'SECKTOR',
-  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
 };
 
 
